01f68650a2578505a00428dfc405ed359c74936f
Reianna <rl3176@barnard.edu>
2023-10-18 02:35:55 -0400

    update

--- static/js/movie_logic.js
+++ static/js/movie_logic.js
@@ -1,39 +1,11 @@
+// --------------- UTILITY FUNCTIONS ---------------
 
-// Load the json data
-var movieData = "http://127.0.0.1:5000/api/read_all";
+// Clears the histogram container
+function clearHistogram() {
+    d3.select("#ratings").html("");
+}
 
-var moviejson;
-
-const movieSelect = d3.select("#selMovies");
-
-// Create a marker layer group.
-let markers = L.layerGroup();
-
-// Get the data with d3.
-d3.json(movieData).then(function (data) {
-    moviejson = data;
-
-    // Append options to the select element
-    for (let i = 0; i < moviejson.length; i++) {
-        let movie = moviejson[i]
-        movieSelect.append("option").text(movie.title).property("value", movie.title);
-        if (i === 0 && movie.ratingsHistograms) {
-            let histSelect = d3.select("#selHist");
-
-            // Append options to the select element
-            Object.keys(movie.ratingsHistograms).forEach(key => {
-                histSelect.append("option").text(key).property("value", key);
-            });
-        }
-    }
-
-    initMap();
-    initSoundtracks();
-
-    let defaultHist = Object.keys(moviejson[0].ratingsHistograms)[0];
-    ratingChanged(defaultHist);
-});
-
+// Append rows to the soundtracks table 
 function appendRowsToTable(data) {
     // Select the table body 
     let tbody = d3.select("#soundtracks").select("tbody");
@@ -56,72 +28,7 @@ function appendRowsToTable(data) {
     }
 }
 
-// Display the default plot
-function initMap() {
-    let movie = moviejson[0];
-    let map = L.map("map", {
-        center: [0, 0],
-        zoom: 2
-    });
-
-    // Create the tile layer that will be the background of our map.
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-
-    // Clear the markers
-    markers.clearLayers();
-
-
-    // Create markers for each location in the dataset
-    movie.locations.forEach(location => {
-        let marker = L.marker([location.lat, location.lon]).bindPopup(`<p>${location.location_name}</p>`);
-        markers.addLayer(marker);
-    });
-
-    // Add our marker cluster layer to the map
-    markers.addTo(map);
-}
-
-function initSoundtracks() {
-    appendRowsToTable(moviejson[0]);
-}
-
-// On change to the dropdown
-d3.selectAll("#selMovies").on("change", optionChanged);
-
-// Function called by DOM changes
-function optionChanged() {
-    let dropdownMenu = d3.select("#selMovies");
-    // Assign the value of the dropdown menu option to a variable
-    let movie = dropdownMenu.property("value");
-
-    let movieData = moviejson.filter(m => m.title === movie)[0];
-
-    let histSelect = d3.select("#selHist");
-
-    // Append options to the select element
-    histSelect.html("");
-    if (movieData.ratingsHistograms && Object.keys(movieData.ratingsHistograms).length > 0) {
-        Object.keys(movieData.ratingsHistograms).forEach(key => {
-            histSelect.append("option").text(key).property("value", key);
-        });
-    } else {
-        histSelect.append("option").text("No histograms available").property("value", "");
-    }
-
-    // Call function to update teh chart
-    updateMap(movieData);
-    updateSoundtracks(movieData);
-}
-
-function ratingChanged(histName) {
-    let selectedMovie = d3.select("#selMovies").property("value");
-    let movieData = moviejson.filter(m => m.title === selectedMovie)[0];
-    let histData = movieData.ratingsHistograms[histName];
-    drawHistogram(histData);
-}
-
+// Updates the map with new data 
 // Update the restyled plot's values
 function updateMap(newdata) {
     // Clear the markers group 
@@ -136,7 +43,10 @@ function updateMap(newdata) {
     }
 }
 
+// Draws the histogram
 function drawHistogram(histData) {
+    clearHistogram();
+
     // Prepare the SVG area
     let svgWidth = 500, svgHeight = 400;
     let margin = { top: 20, right: 20, bottom: 30, left: 40 };
@@ -186,3 +96,113 @@ function drawHistogram(histData) {
 function updateSoundtracks(newdata) {
     appendRowsToTable(newdata);
 }
+
+// --------------- INIT FUNCTIONS ---------------
+// Initailizes the map
+function initMap(movie) {
+    let map = L.map("map", {
+        center: [0, 0],
+        zoom: 2
+    });
+
+    // Create the tile layer that will be the background of our map.
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    // Clear the markers
+    markers.clearLayers();
+
+    if (movie.locations && movie.locations.length > 0) {
+        // Create markers for each location in the dataset
+        movie.locations.forEach(location => {
+            let marker = L.marker([location.lat, location.lon]).bindPopup(`<p>${location.location_name}</p>`);
+            markers.addLayer(marker);
+        });
+
+        // Add our marker cluster layer to the map
+        markers.addTo(map);
+    }
+}
+
+// Initializes the soundtracks table
+function initSoundtracks(movie) {
+    appendRowsToTable(movie);
+}
+
+// --------------- EVENT HANDLERS ---------------
+// Handles the event when a new movie is selected
+function optionChanged() {
+    let dropdownMenu = d3.select("#selMovies");
+    // Assign the value of the dropdown menu option to a variable
+    let movie = dropdownMenu.property("value");
+
+    let movieData = moviejson.filter(m => m.title === movie)[0];
+
+    let histSelect = d3.select("#selHist");
+
+    // Append options to the select element
+    histSelect.html("");
+    if (movieData.ratingsHistograms && Object.keys(movieData.ratingsHistograms).length > 0) {
+        Object.keys(movieData.ratingsHistograms).forEach(key => {
+            histSelect.append("option").text(key).property("value", key);
+        });
+        let defaultHist = Object.keys(movieData.ratingsHistograms)[0];
+        ratingChanged(defaultHist);
+    } else {
+        histSelect.append("option").text("No histograms available").property("value", "");
+    }
+
+    // Call function to update the chart
+    updateMap(movieData);
+    updateSoundtracks(movieData);
+}
+
+// Handles the event when a new histogram is selected
+function ratingChanged(histName) {
+    let selectedMovie = d3.select("#selMovies").property("value");
+    let movieData = moviejson.filter(m => m.title === selectedMovie)[0];
+    let histData = movieData.ratingsHistograms[histName];
+    drawHistogram(histData);
+}
+
+// --------------- DATA LOADING ---------------
+
+var movieData = "http://127.0.0.1:5000/api/read_all";
+var moviejson;
+const movieSelect = d3.select("#selMovies");
+const histSelect = d3.select("#selHist");
+let markers = L.layerGroup();
+
+// Get the data with d3.
+d3.json(movieData).then(function (data) {
+    moviejson = data;
+
+    // Populate movies dropdown
+    moviejson.forEach(movie => {
+        movieSelect.append("option").text(movie.title).property("value", movie.title);
+    });
+
+    // Set the default movie and initialize the visuals 
+    movieSelect.property("value", selectedMovieTitle);
+    let movie = moviejson.filter(m => m.title === selectedMovieTitle)[0];
+    initMap(movie);
+    initSoundtracks(movie);
+
+    // Initialize teh histograms dropdown for the default movie
+    if (movie.ratingsHistograms && Object.keys(movie.ratingsHistograms).length > 0) {
+        Object.keys(movie.ratingsHistograms).forEach(key => {
+            histSelect.append("option").text(key).property("value", key);
+        });
+        let defaultHist = Object.keys(movie.ratingsHistograms)[0];
+        ratingChanged(defaultHist);
+    }
+    else {
+        histSelect.append("option").text("No histograms available").property("value", "");
+    }
+});
+
+
+
+// Register the event handler
+d3.selectAll("#selMovies").on("change", optionChanged);
